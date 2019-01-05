2cf104864d7df126f595052be011c79b32e28e39
Kentaro Okushiro <ken09460308@gmail.com>
2019-01-05 20:58:15 +0900

    Initial Commit

---
+++ 05_08_okushirokentaro/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  05_08_okushirokentaro
+//
+//  Created by 奥城健太郎 on 2019/01/05.
+//  Copyright © 2019年 奥城健太郎. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
